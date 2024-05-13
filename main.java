4981cc720667e1f330d8ec0705f37c3f687d8f78
Samuel Madrid <madridossa@gmail.com>
2024-05-12 20:05:45 -0500

    Commit de develop

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String[] args){
+        saludar();
+    }
+    public static void saludar(){
+        System.out.println("Assignment 3 by Samuel Madrid Ossa");
+    }
+}
